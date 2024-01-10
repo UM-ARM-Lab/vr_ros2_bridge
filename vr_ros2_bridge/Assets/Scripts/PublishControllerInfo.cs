24e777271c56aa9fbdb1e5070101fb556d72611d
VRUser <VRUser@eecs-armor>
2024-01-10 12:13:50 -0500

    bring back inputactions map from the samples

--- vr_ros2_bridge/Assets/Scripts/PublishControllerInfo.cs
+++ vr_ros2_bridge/Assets/Scripts/PublishControllerInfo.cs
@@ -3,6 +3,7 @@ using Unity.Robotics.ROSTCPConnector;
 using System.Collections.Generic;
 using UnityEngine.InputSystem.XR;
 using RosMessageTypes.VrRos2Bridge;
+using RosMessageTypes.Geometry;
 
 /// <summary>
 ///
@@ -11,14 +12,13 @@ public class PublishControllerInfo : MonoBehaviour
 {
     ROSConnection ros;
     public string topicName = "vr_controller_info";
-    ControllersInfoMsg controllersInfoMsg;
 
     // Controller Input Devices - used to access button states.
     List<UnityEngine.XR.InputDevice> controllers = new List<UnityEngine.XR.InputDevice>();
 
     // Controller Game Objects - used to access controller tracked poses.
-    GameObject leftController;
-    GameObject rightController;
+    public GameObject leftController;
+    public GameObject rightController;
 
     void Start()
     {
@@ -29,6 +29,8 @@ public class PublishControllerInfo : MonoBehaviour
 
     private void Update()
     {
+        ControllersInfoMsg controllersInfoMsg = new ControllersInfoMsg();
+
         var desiredCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Controller;
         UnityEngine.XR.InputDevices.GetDevicesWithCharacteristics(desiredCharacteristics, controllers);
 
@@ -36,13 +38,24 @@ public class PublishControllerInfo : MonoBehaviour
 
         foreach (var device in controllers)
         {
-            Debug.Log(string.Format("Device found with name '{0}' and role '{1}'", device.name, device.role.ToString()));
-            var name = device.role.ToString();  // TODO: Redo naming using characteristics.
+            bool isLeftController = (device.characteristics & UnityEngine.XR.InputDeviceCharacteristics.Left) != UnityEngine.XR.InputDeviceCharacteristics.None;
+            // Debug.Log(string.Format("Device found with name '{0}' and role '{1}'.", device.name, device.role.ToString()));
+            var name = isLeftController ? "LeftController" : "RightController";
 
-            // TODO: Get pose of the controller, based on whether its the left/right controller.
-            
             var controllerInfoMsg = new ControllerInfoMsg();
+            controllerInfoMsg.controller_name = name;
 
+            // Get pose of the controller, based on whether its the left/right controller.
+            GameObject thisController = isLeftController ? leftController : rightController;
+            controllerInfoMsg.controller_pose.position.x = thisController.transform.position.x;
+            controllerInfoMsg.controller_pose.position.y = thisController.transform.position.y;
+            controllerInfoMsg.controller_pose.position.z = thisController.transform.position.z;
+            controllerInfoMsg.controller_pose.orientation.w = thisController.transform.rotation.w;
+            controllerInfoMsg.controller_pose.orientation.x = thisController.transform.rotation.x;
+            controllerInfoMsg.controller_pose.orientation.y = thisController.transform.rotation.y;
+            controllerInfoMsg.controller_pose.orientation.z = thisController.transform.rotation.z;
+
+            // Get button press states.
             bool triggerValue = false;
             if (device.TryGetFeatureValue(UnityEngine.XR.CommonUsages.triggerButton, out triggerValue) && triggerValue)
             {
@@ -67,8 +80,19 @@ public class PublishControllerInfo : MonoBehaviour
                 Debug.Log(string.Format("{0}: Trackpad button is pressed!", name));
             }
 
-            // TODO: Populate controller info.
+            // Populate controller button info.
+            controllerInfoMsg.trigger_button = triggerValue;
+            controllerInfoMsg.trigger_axis = triggerAxisValue;
+            controllerInfoMsg.grip_button = gripValue;
+            controllerInfoMsg.trackpad_button = primary2DAxisClickValue;
+
+            // Add to list of controller info messages.
+            controllerInfoMsgs.Add(controllerInfoMsg);
         }
+
+        // Populate controllers info msg.
+        controllersInfoMsg.controllers_info = controllerInfoMsgs.ToArray();
+        // TODO: Add header info.
 
         // Finally send the message to server_endpoint.py running in ROS
         ros.Publish(topicName, controllersInfoMsg);
